c083ae86122fbaf1db22f3b2c021af987e6474b2
theredguy2006 <theredguy2006@gmail.com>
2026-02-09 15:10:08 +0530

    Initial Commit for  Journaling app

---
+++ Journaling App/src/main/java/com/red/journalingapp/JournalingAppApplication.java
@@ -0,0 +1,13 @@
+package com.red.journalingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalingAppApplication.class, args);
+    }
+
+}
