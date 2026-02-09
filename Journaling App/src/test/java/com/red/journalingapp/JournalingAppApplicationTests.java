8cbb669ed2b091e547d198cfb8f307b4dd3c48bf
theredguy2006 <theredguy2006@gmail.com>
2026-02-09 15:10:08 +0530

    Initial Commit for  Journaling app

---
+++ Journaling App/src/test/java/com/red/journalingapp/JournalingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red.journalingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
