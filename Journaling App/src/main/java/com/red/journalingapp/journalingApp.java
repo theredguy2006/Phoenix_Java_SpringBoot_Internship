96b13c9d95d641562d6963084ff0b1f431ce27e9
RedGuy2006 <theredguy2006@gmail.com>
2026-02-10 13:00:22 +0530

    "Merging into Main"

---
+++ Journaling App/src/main/java/com/red/journalingapp/journalingApp.java
@@ -0,0 +1,13 @@
+package com.red.journalingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class journalingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(journalingApp.class, args);
+    }
+
+}
