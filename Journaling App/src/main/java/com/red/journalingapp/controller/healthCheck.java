a19d93b9f7ac05dd92da74166ccbf3af29281b46
RedGuy2006 <theredguy2006@gmail.com>
2026-02-10 12:57:05 +0530

    "First Commit for the journalingApp project. "

---
+++ Journaling App/src/main/java/com/red/journalingapp/controller/healthCheck.java
@@ -0,0 +1,15 @@
+package com.red.journalingapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthCheck {
+
+
+//    in postman if we call this method via post it will give error and not allowed.
+    @GetMapping("/health-check")
+    public String healthcheck() {
+        return "okay";
+    }
+}
