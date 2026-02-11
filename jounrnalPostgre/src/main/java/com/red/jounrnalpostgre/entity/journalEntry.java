6b0b89910440a5cb9e0190025544ba2da498d72c
RedGuy2006 <theredguy2006@gmail.com>
2026-02-11 15:59:00 +0530

    "First Commit For the pgAdmin version of journalingApp"

---
+++ jounrnalPostgre/src/main/java/com/red/jounrnalpostgre/entity/journalEntry.java
@@ -0,0 +1,50 @@
+package com.red.jounrnalpostgre.entity;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public class journalEntry {
+    //    @NotEmpty(message = "You cannot create a journal without this")
+//    This only works on String , Arrays , Collections and etc not for other values.
+    @NotNull(message = "Id cannot be null ")
+    private long id;
+    @NotNull(message = "Title cannot be null ")
+    private String title;
+    @NotNull(message = "content cannot be null ")
+    private String content;
+    @Email(message = "Please Enter a Valid Email Id ")
+//    Somehow adding a ';' will raise/create an error.
+    private String emailId;
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
