755fd941feae4a5c29134cb0b8bfb9bdcdc6a21c
RedGuy2006 <theredguy2006@gmail.com>
2026-02-12 14:06:34 +0530

    journalPostgre (#2)
    * "First Commit For the pgAdmin version of journalingApp"
    
    * "Second Commit , working version "
    
    * "Repo and Service Files "

---
+++ jounrnalPostgre/src/main/java/com/red/jounrnalpostgre/JounrnalPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.red.jounrnalpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JounrnalPostgreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JounrnalPostgreApplication.class, args);
+    }
+
+}
