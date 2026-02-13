8ba25fe159fc6a8eb2b237ca1e010b68ab636acc
RedGuy2006 <theredguy2006@gmail.com>
2026-02-13 14:47:45 +0530

    "Changed Folder Structure"

---
+++ Day 4/ResponseEntity/src/main/java/com/red/responseentity/service/JournalService.java
@@ -0,0 +1,36 @@
+package com.red.responseentity.service;
+
+import com.red.responseentity.entity.JournalEntry;
+import com.red.responseentity.repository.JournalRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalService {
+
+    @Autowired
+    private JournalRepo journalRepo;
+
+    public void saveJournal(JournalEntry journalEntry) {
+        journalRepo.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAllJournal() {
+        return journalRepo.findAll();
+
+    }
+
+
+    public Optional<JournalEntry> getJournalById(Long id) {
+        return journalRepo.findById(id);
+    }
+
+    public void deleteByID(Long id) {
+        journalRepo.deleteById(id);
+    }
+}
+
+// controller --> service --> repository
