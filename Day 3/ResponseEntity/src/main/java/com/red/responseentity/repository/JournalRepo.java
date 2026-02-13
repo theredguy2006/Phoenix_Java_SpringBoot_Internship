6adce6c753b15365d6c4b8f083887ab3551e249f
RedGuy2006 <theredguy2006@gmail.com>
2026-02-13 14:45:59 +0530

    "Changed Folder Structure"

--- Day 3/ResponseEntity/src/main/java/com/red/responseentity/repository/JournalRepo.java
+++
@@ -1,7 +0,0 @@
-package com.red.responseentity.repository;
-
-import com.red.responseentity.entity.JournalEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JournalRepo extends JpaRepository<JournalEntry, Long> {
-}
