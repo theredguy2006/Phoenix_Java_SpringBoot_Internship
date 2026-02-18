d2a74f36035c034c149b6bce16169666e931dec7
RedGuy2006 <theredguy2006@gmail.com>
2026-02-18 16:57:11 +0530

    "Working Version of Blog Db(Not tested yet) Although Running Fine. Updated Comment Controller and Service  "

--- Day 7/blog_db/src/main/java/com/red/blog_db/controller/CommentController.java
+++ Day 7/blog_db/src/main/java/com/red/blog_db/controller/CommentController.java
@@ -1,10 +1,39 @@
 package com.red.blog_db.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.red.blog_db.entity.CommentEntity;
+import com.red.blog_db.entity.PostEntity;
+import com.red.blog_db.service.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping
+    public List<CommentEntity> getAllComments() {
+        return commentService.getAllComments();
+    }
+
+    @GetMapping("/id/{myId}")
+    public Optional<CommentEntity> getPostById(@PathVariable Long myId) {
+        return commentService.getCommentById(myId);
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public void deletePostById(@PathVariable Long myId) {
+        commentService.deleteComment(myId);
+    }
+
+    @PostMapping("/posttitle/{postTitle}/username/{username}")
+    public void createPost(@Valid @RequestBody CommentEntity commentEntity, @PathVariable String userName, @PathVariable String postTitle) {
+        commentService.createComment(postTitle, userName , commentEntity);
+    }
+
 }
