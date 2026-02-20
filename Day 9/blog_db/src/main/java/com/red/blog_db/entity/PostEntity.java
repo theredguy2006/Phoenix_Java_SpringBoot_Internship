fdcc8642ba059b38c27746ac45b40cf658d486e0
RedGuy2006 <theredguy2006@gmail.com>
2026-02-20 11:31:51 +0530

    "Initializing Day 9 Branch.  "

---
+++ Day 9/blog_db/src/main/java/com/red/blog_db/entity/PostEntity.java
@@ -0,0 +1,48 @@
+package com.red.blog_db.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @NotNull
+    @Column(name = "post_title")
+    private String postTitle;
+
+    @NotNull
+    @Column(name = "post_body")
+    private String postBody;
+
+    @Column(name = "post_time")
+    private LocalDateTime postTime;
+
+    // 🔹 POST → COMMENTS
+    @OneToMany(mappedBy = "postEntity", cascade = CascadeType.ALL)
+    @JsonManagedReference(value = "post-comment")
+    private List<CommentEntity> commentEntityList = new ArrayList<>();
+
+    // 🔹 POST → USER (BACK SIDE OF user-post)
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference(value = "user-post")
+    private UserEntity userEntity;
+}
---
+++ Day 9/blog_db/src/main/java/com/red/blog_db/service/UserService.java
@@ -0,0 +1,51 @@
+package com.red.blog_db.service;
+
+import com.red.blog_db.entity.UserEntity;
+import com.red.blog_db.repository.UserRepository;
+import jakarta.validation.constraints.NotNull;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserEntity> getAllUsers() {
+        return new ArrayList<>(userRepository.findAll());
+    }
+
+    public UserEntity getUserById(Long myId) {
+        return userRepository.findByUserId(myId);
+    }
+
+    public void createUser(@NonNull UserEntity userEntity) {
+        userEntity.setCreatedAt(LocalDateTime.now());
+        userRepository.save(userEntity);
+    }
+
+    public void updateUserByUsername(@NotNull @NonNull UserEntity userEntity, Long myId) {
+
+        UserEntity user = userRepository.findByUserId(myId);
+        user.setUserName(userEntity.getUserName());
+        user.setUserPwd(userEntity.getUserPwd());
+        user.setEmailId(userEntity.getEmailId());
+        userRepository.save(user);
+    }
+
+    public void deleteUser(Long myId) {
+
+        if (!userRepository.existsById(myId)) {
+            throw new RuntimeException("User not found");
+        }
+        userRepository.deleteById(myId);
+    }
+
+
+}
