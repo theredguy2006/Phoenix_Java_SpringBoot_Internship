70305813d5854480210c65949e3466be524a6f8b
RedGuy2006 <theredguy2006@gmail.com>
2026-02-23 12:58:12 +0530

    "Initializing Day 10 branch. "

---
+++ Day 10/userExceptions/src/main/java/com/red/userexceptions/entity/UserEntity.java
@@ -0,0 +1,13 @@
+package com.red.userexceptions.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+}
---
+++ Day 10/userExceptions/src/main/java/com/red/userexceptions/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.red.userexceptions.mapper;
+
+public class UserMapper {
+}
