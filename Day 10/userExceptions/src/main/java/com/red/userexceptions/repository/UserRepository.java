2a47deef68b07ed9e7725db9b263622f025ac440
RedGuy2006 <theredguy2006@gmail.com>
2026-02-25 17:12:57 +0530

    "Implemented Security And Role Based Operations "

--- Day 10/userExceptions/src/main/java/com/red/userexceptions/repository/UserRepository.java
+++ Day 10/userExceptions/src/main/java/com/red/userexceptions/repository/UserRepository.java
@@ -4,9 +4,13 @@ import com.red.userexceptions.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUserId(Long userId);
 
     boolean existsByEmailId(String emailId);
+
+    Optional<UserEntity> findByEmailId(String emailId);
 }
